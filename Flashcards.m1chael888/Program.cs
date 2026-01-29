1ce277338710d58fb20464461696f5e8e72cf6e3
m1chael888 <sofishmain@gmail.com>
2026-01-29 09:23:17 +0200

    setup

--- Flashcards.m1chael888/Program.cs
+++ Flashcards.m1chael888/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Flashcards.m1chael888
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flashcards.m1chael888
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json")
+                .Build();
+            var connectionString = builder.GetConnectionString("source");
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.Add
         }
     }
 }
